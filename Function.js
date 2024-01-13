a626d7398bfecaf243feaac9e367c73beb6f9d77
mahar noor <maharnoor380@gmail.com>
2024-01-13 12:54:31 +0500

    loop add

---
+++ Function.js
@@ -0,0 +1,12 @@
+const printDetails = (name, number) => {
+   
+    console.log("The name is " + name);
+  
+
+    console.log(`Table of ${number}:`);
+    for (let i = 1; i <= 10; i++) {
+      console.log(`${number} * ${i} = ${number * i}`);
+    }
+  
+}
+  
